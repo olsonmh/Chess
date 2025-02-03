79acbbeee82b98afbbb5fee68b05688bc267b7d4
Micah H Olson <olsonmh@byu.edu>
2025-02-03 13:34:41 -0700

    Delete old comments

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,12 +1,6 @@
 package chess;
 import java.util.Objects;
 
-/**
- * Represents a single square position on a chess board
- * <p>
- * Note: You can add to this class, but you may not alter
- * signature of the existing methods.
- */
 public class ChessPosition {
 
     private final int col;
@@ -17,19 +11,11 @@ public class ChessPosition {
         this.row = row;
     }
 
-    /**
-     * @return which row this position is in
-     * 1 codes for the bottom row
-     */
     public int getRow() {
         return row;
 
     }
 
-    /**
-     * @return which column this position is in
-     * 1 codes for the left row
-     */
     public int getColumn() {
         return col;
     }
