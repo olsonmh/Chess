db45b9d5ebdbd1464f6f73668b4df49ec130f397
Micah H Olson <olsonmh@byu.edu>
2025-02-24 21:33:18 -0700

    Start methods for UserDAO class

--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -1,4 +1,18 @@
 package dataaccess;
+import model.UserData;
 
 public class UserDAO {
+
+
+    public void createUser(UserData user){
+    }
+    public UserData getUser(String username){
+        throw new RuntimeException();
+    }
+    public void clearUserData(){
+    }
+
 }
+
+
+//void insertUser(UserData u) throws DataAccessException;
