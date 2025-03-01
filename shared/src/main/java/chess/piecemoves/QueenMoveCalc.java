c8cf4a11f41885613034561116334ec71348454c
Micah H Olson <olsonmh@byu.edu>
2025-03-01 14:17:49 -0700

    Add refactor to delete duplicate code

--- shared/src/main/java/chess/piecemoves/QueenMoveCalc.java
+++ shared/src/main/java/chess/piecemoves/QueenMoveCalc.java
@@ -2,6 +2,7 @@ package chess.piecemoves;
 
 import chess.ChessBoard;
 import chess.ChessMove;
+import chess.ChessPiece;
 import chess.ChessPosition;
 
 import java.util.Collection;
@@ -9,63 +10,11 @@ import java.util.HashSet;
 
 public class QueenMoveCalc extends MoveCalc {
     public Collection<ChessMove> pieceMoves2(ChessBoard board, ChessPosition myPosition){
-        Collection<ChessMove> validMoves = new HashSet<>();
-        for(int j=-1; j<=1; j+=2){
-            for(int dy=1; dy<=8; ++dy){
-                ChessPosition pose = new ChessPosition(myPosition.getRow()+(dy*j), myPosition.getColumn());
-                if(inBounds(pose, myPosition)){
-                    if(board.getPiece(pose) != null){
-                        if(board.getPiece(pose).getTeamColor() != board.getPiece(myPosition).getTeamColor()){
-                            validMoves.add(new ChessMove(myPosition, pose, null));
-                        }
-                        break;
-                    }
-                    else{validMoves.add(new ChessMove(myPosition, pose, null));}
-                }
-            }
-        }
-        for(int j=-1; j<=1; j+=2){
-            for(int dx=1; dx<=8; ++dx){
-                ChessPosition pose = new ChessPosition(myPosition.getRow(), myPosition.getColumn()+(dx*j));
-                if(inBounds(pose, myPosition)){
-                    if(board.getPiece(pose) != null){
-                        if(board.getPiece(pose).getTeamColor() != board.getPiece(myPosition).getTeamColor()){
-                            validMoves.add(new ChessMove(myPosition, pose, null));
-                        }
-                        break;
-                    }
-                    else{validMoves.add(new ChessMove(myPosition, pose, null));}
-                }
-            }
-        }
-        for(int j=-1; j<=1; j+=2){
-            for(int d = 1; d<=8; ++d){
-                ChessPosition pose = new ChessPosition(myPosition.getRow()+(d*j), myPosition.getColumn()+(d*j));
-                if(inBounds(pose, myPosition)){
-                    if(board.getPiece(pose) != null){
-                        if(board.getPiece(pose).getTeamColor() != board.getPiece(myPosition).getTeamColor()){
-                            validMoves.add(new ChessMove(myPosition, pose, null));
-                        }
-                        break;
-                    }
-                    else{validMoves.add(new ChessMove(myPosition, pose, null));}
-                }
-            }
-        }
-        for(int j=-1; j<=1; j+=2){
-            for(int d = 1; d<=8; ++d){
-                ChessPosition pose = new ChessPosition(myPosition.getRow()-(d*j), myPosition.getColumn()+(d*j));
-                if(inBounds(pose, myPosition)){
-                    if(board.getPiece(pose) != null){
-                        if(board.getPiece(pose).getTeamColor() != board.getPiece(myPosition).getTeamColor()){
-                            validMoves.add(new ChessMove(myPosition, pose, null));
-                        }
-                        break;
-                    }
-                    else{validMoves.add(new ChessMove(myPosition, pose, null));}
-                }
-            }
-        }
-        return validMoves;
+        MoveCalc moves = new RookMoveCalc();
+        Collection<ChessMove> validMoves1 = moves.pieceMoves2(board, myPosition);
+        MoveCalc moves2 = new BishopMoveCalc();
+        Collection<ChessMove> validMoves2 = moves2.pieceMoves2(board, myPosition);
+        validMoves1.addAll(validMoves2);
+        return validMoves1;
     }
 }
