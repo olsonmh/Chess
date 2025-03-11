506969b0bec1ec1fd762ca61d61ae67327ded284
Micah H Olson <olsonmh@byu.edu>
2025-03-11 09:45:19 -0600

    Submit changes to mysql statements

--- server/src/main/java/dataaccess/MySqlDataAccess.java
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -15,7 +15,6 @@ import java.util.HashSet;
 import static java.sql.Statement.RETURN_GENERATED_KEYS;
 import static java.sql.Types.NULL;
 
-import org.eclipse.jetty.server.Authentication;
 import org.mindrot.jbcrypt.BCrypt;
 
 
@@ -27,19 +26,18 @@ public class MySqlDataAccess implements GameDAO, UserDAO, AuthDAO {
 
     @Override
     public UserData getUser(String username){
-        String statement = "SELECT name, passHash, email FROM user WHERE username = '%s';".formatted(username);
-        ResultSet results = executeQuery(statement);
-        return new UserData(results.getString(0),results.getString(1),results.getString(2));
+        String statement = "SELECT name, passHash, email FROM user WHERE name = '%s';".formatted(username);
+        try {
+            ResultSet results = executeQuery(statement);
+            return new UserData(results.getString(0), results.getString(1), results.getString(2));
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
     }
 
     @Override
     public void clearUserData(){
-        try{
         executeStatement("DELETE FROM user;");
-        } catch (SQLException | DataAccessException e){
-            throw new RuntimeException(e);
-        }
-
     }
 
     @Override
@@ -47,7 +45,6 @@ public class MySqlDataAccess implements GameDAO, UserDAO, AuthDAO {
         String hashedPassword = BCrypt.hashpw(user.password(), BCrypt.gensalt());
         String statement = "INSERT INTO user (name, passHash, email) VALUES ('%s','%s','%s');"
                             .formatted(user.username(), hashedPassword, user.email());
-
         executeStatement(statement);
     }
 
@@ -62,61 +59,96 @@ public class MySqlDataAccess implements GameDAO, UserDAO, AuthDAO {
     @Override
     public GameData getGame(int gameID){
         String statement = "SELECT id, whiteUser, blackUser, name, state FROM game WHERE id = %d;".formatted(gameID);
-        ResultSet results = executeQuery(statement);
-
-        return new GameData(results.getInt(0),
-                            results.getString(1),
-                            results.getString(2),
-                            results.getString(3),
-                            new Gson().fromJson(results.getString(4), ChessGame.class));
+        try {
+            ResultSet results = executeQuery(statement);
+            return new GameData(results.getInt(0),
+                                results.getString(1),
+                                results.getString(2),
+                                results.getString(3),
+                                new Gson().fromJson(results.getString(4), ChessGame.class));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Collection<GameDataForListing> listGames(){
         Collection<GameDataForListing> listOfGames = new HashSet<>();
         String statement = "SELECT id, whiteUser, blackUser, name FROM game;";
-        ResultSet results = executeQuery(statement);
-        while(results.next()){
-            listOfGames.add(new GameDataForListing(results.getInt(0),
-                                                   results.getString(1),
-                                                   results.getString(2),
-                                                   results.getString(3)));
+        try{
+            ResultSet results = executeQuery(statement);
+            while(results.next()){
+                listOfGames.add(new GameDataForListing(results.getInt(0),
+                                                       results.getString(1),
+                                                       results.getString(2),
+                                                       results.getString(3)));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
         return listOfGames;
     }
 
     @Override
-    public void updateGame(GameData gameData){}
+    public void updateGame(GameData gameData){
+        String jsonState = new Gson().toJson(gameData.game());
+        String statement = "UPDATE game SET state = '%s' WHERE id = %d;".formatted(jsonState, gameData.gameID());
+        executeStatement(statement);
+    }
 
     @Override
-    public void clearGames(){}
+    public void clearGames(){
+        executeStatement("DELETE FROM game;");
+    }
 
     @Override
-    public void createAuth(AuthData authdata){}
+    public void createAuth(AuthData authdata){
+        String statement = "INSERT INTO auth (token, user) VALUES ('%s','%s');"
+                            .formatted(authdata.authToken(), authdata.username());
+        executeStatement(statement);
+    }
 
     @Override
     public AuthData getAuth(String authToken) {
-        return null;
+        String statement = "SELECT token, user FROM auth WHERE token = '%s';".formatted(authToken);
+        ResultSet results = executeQuery(statement);
+        try {
+            return new AuthData(results.getString(0), results.getString(1));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
-    public void deleteAuth(String authToken){}
+    public void deleteAuth(String authToken){
+        executeStatement("DELETE FROM auth WHERE token = '%s';".formatted(authToken));
+    }
 
     @Override
-    public void clearAuthTokens(){}
-
-
-    private ResultSet executeQuery(String statement) throws DataAccessException, SQLException {
-        var connection = DatabaseManager.getConnection();
-        var prepareStatement = connection.prepareStatement(statement);
-        return prepareStatement.executeQuery();
+    public void clearAuthTokens(){
+        executeStatement("DELETE FROM auth;");
 
     }
 
-    private void executeStatement(String statement) throws DataAccessException, SQLException {
-        var connection = DatabaseManager.getConnection();
-        var prepareStatement = connection.prepareStatement(statement);
-        prepareStatement.execute();
+
+    private ResultSet executeQuery(String statement) {
+        try {
+            var connection = DatabaseManager.getConnection();
+            var prepareStatement = connection.prepareStatement(statement);
+            return prepareStatement.executeQuery();
+        } catch (DataAccessException | SQLException e){
+            throw new RuntimeException();
+        }
+    }
+
+    private void executeStatement(String statement) {
+        try {
+            var connection = DatabaseManager.getConnection();
+            var prepareStatement = connection.prepareStatement(statement);
+            prepareStatement.execute();
+        } catch (DataAccessException | SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private int executeUpdate(String statement, Object... params) {
@@ -126,7 +158,7 @@ public class MySqlDataAccess implements GameDAO, UserDAO, AuthDAO {
                     var param = params[i];
                     if (param instanceof String p) ps.setString(i + 1, p);
                     else if (param instanceof Integer p) ps.setInt(i + 1, p);
-                    else if (param instanceof PetType p) ps.setString(i + 1, p.toString());
+                    //else if (param instanceof PetType p) ps.setString(i + 1, p.toString());
                     else if (param == null) ps.setNull(i + 1, NULL);
                 }
                 ps.executeUpdate();
@@ -168,7 +200,20 @@ public class MySqlDataAccess implements GameDAO, UserDAO, AuthDAO {
     };
 
 
-    private void configureDatabase() throws RuntimeException{
+    private void configureDatabase(){
+        try{
+            DatabaseManager.createDatabase();
+            var connection = DatabaseManager.getConnection();
+            for(var statement : createStatements){
+                var preparedStatement = connection.prepareStatement(statement);
+                preparedStatement.executeUpdate();
+            }
+        } catch (DataAccessException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    /**
+    private void configureDatabase(){
         DatabaseManager.createDatabase();
         try (var conn = DatabaseManager.getConnection()) {
             for (var statement : createStatements) {
@@ -179,5 +224,5 @@ public class MySqlDataAccess implements GameDAO, UserDAO, AuthDAO {
         } catch (SQLException ex) {
             throw new ResponseException(500, String.format("Unable to configure database: %s", ex.getMessage()));
         }
-    }
+    }**/
 }
