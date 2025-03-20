509a26e4705bba68f38a3cf1ed02578237dc67c9
Micah H Olson <olsonmh@byu.edu>
2025-03-19 20:37:28 -0600

    add server facade endpoints

--- client/src/main/java/ServerFacade.java
+++ client/src/main/java/ServerFacade.java
@@ -1,2 +1,17 @@
 public class ServerFacade {
+
+    public void register(){}
+
+    public void login(){}
+
+    public void logout(){}
+
+    public void createGame(){}
+
+    public void joinGame(){}
+
+    public void listGames(){}
+
+    public void clearAll(){}
+
 }
