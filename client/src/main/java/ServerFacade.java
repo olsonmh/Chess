c30801ad67194754af710beba8f293df3bea74ec
Micah H Olson <olsonmh@byu.edu>
2025-03-21 22:09:14 -0600

    delete small stuff

--- client/src/main/java/ServerFacade.java
+++ client/src/main/java/ServerFacade.java
@@ -14,46 +14,4 @@ public class ServerFacade {
 
     public void clearAll(){}
 
-
-    private final GameService gameService;
-
-    public GameController(GameService gameService) {
-        this.gameService = gameService;
-    }
-    /*
-    @PostMapping("/register")
-    public void register(@RequestBody User user) {
-        gameService.register(user);
-    }
-
-    @PostMapping("/login")
-    public void login(@RequestBody User credentials) {
-        gameService.login(credentials);
-    }
-
-    @PostMapping("/logout")
-    public void logout() {
-        gameService.logout();
-    }
-
-    @PostMapping("/createGame")
-    public void createGame(@RequestBody Game game) {
-        gameService.createGame(game);
-    }
-
-    @PostMapping("/joinGame")
-    public void joinGame(@RequestParam String gameId, @RequestBody User player) {
-        gameService.joinGame(gameId, player);
-    }
-
-    @GetMapping("/listGames")
-    public List<Game> listGames() {
-        return gameService.listGames();
-    }
-
-    @DeleteMapping("/clearAll")
-    public void clearAll() {
-        gameService.clearAll();
-    }
-    */
 }
