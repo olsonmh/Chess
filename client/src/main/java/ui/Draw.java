307d3716d2d5b75bf7b3f0271e0b91ec851e5b85
Micah H Olson <olsonmh@byu.edu>
2025-04-10 00:40:19 -0600

    fix chess board orientation

--- client/src/main/java/ui/Draw.java
+++ client/src/main/java/ui/Draw.java
@@ -42,9 +42,11 @@ public class Draw {
             System.out.printf("%s %d ", EscapeSequences.SET_BG_COLOR_LIGHT_GREY, row+1);
 
             for(int j = 0; j<=7; j++){
-                int row2 = flipBoard ? 8 -i : i+1;
+                int col = flipBoard ? j : 7 - j;
+                int row2 = flipBoard ? 8 - i : i+1;
+                int col2 = flipBoard ? j+1 : 8 -j;
                 if (!positions.isEmpty()){
-                    if (positions.contains(new ChessPosition(row2,j+1))) {
+                    if (positions.contains(new ChessPosition(row2,col2))) {
                         backgroundColor = (i + j) % 2 == 0 ? EscapeSequences.SET_BG_COLOR_GREEN_TINT_WHITE : EscapeSequences.SET_BG_COLOR_GREEN_TINT_BLACK;
                     } else {
                         backgroundColor = (i + j) % 2 == 0 ? EscapeSequences.SET_BG_COLOR_WHITE : EscapeSequences.SET_BG_COLOR_BLACK;
@@ -53,13 +55,13 @@ public class Draw {
                     backgroundColor = (i + j) % 2 == 0 ? EscapeSequences.SET_BG_COLOR_WHITE : EscapeSequences.SET_BG_COLOR_BLACK;
                 }
                 if (pose != null){
-                    if (pose.equals(new ChessPosition(row2,j+1))){
+                    if (pose.equals(new ChessPosition(row2,col2))){
                         backgroundColor = (i + j) % 2 == 0 ? EscapeSequences.SET_BG_COLOR_YELLOW : EscapeSequences.SET_BG_COLOR_YELLOW_BLACK;
                     }
                 }
 
                 String label;
-                ChessPiece piece = game.getBoard().board[row][j];
+                ChessPiece piece = game.getBoard().board[row][col];
                 if (piece == null) {
                     label = "   ";
                 } else {
@@ -111,36 +113,18 @@ public class Draw {
     public static ChessPosition getPose(String piece, String color){
         String firstLetter = piece.substring(0, 1);
         String secondLetter = piece.substring(1, 2);
-        Map<String, Integer> letterToNumberWhite = new HashMap<>();
+        Map<String, Integer> letterToNumber = new HashMap<>();
 
-        letterToNumberWhite.put("a", 1);
-        letterToNumberWhite.put("b", 2);
-        letterToNumberWhite.put("c", 3);
-        letterToNumberWhite.put("d", 4);
-        letterToNumberWhite.put("e", 5);
-        letterToNumberWhite.put("f", 6);
-        letterToNumberWhite.put("g", 7);
-        letterToNumberWhite.put("h", 8);
+        letterToNumber.put("a", 1);
+        letterToNumber.put("b", 2);
+        letterToNumber.put("c", 3);
+        letterToNumber.put("d", 4);
+        letterToNumber.put("e", 5);
+        letterToNumber.put("f", 6);
+        letterToNumber.put("g", 7);
+        letterToNumber.put("h", 8);
 
-        Map<String, Integer> letterToNumberBlack = new HashMap<>();
-
-        letterToNumberBlack.put("a", 8);
-        letterToNumberBlack.put("b", 7);
-        letterToNumberBlack.put("c", 6);
-        letterToNumberBlack.put("d", 5);
-        letterToNumberBlack.put("e", 4);
-        letterToNumberBlack.put("f", 3);
-        letterToNumberBlack.put("g", 2);
-        letterToNumberBlack.put("h", 1);
-
-        int secondNumber;
-        if (color != null && color.equalsIgnoreCase("black")){
-            secondNumber = letterToNumberBlack.get(firstLetter);
-        } else {
-            secondNumber = letterToNumberWhite.get(firstLetter);
-        }
-
-        //int secondNumber = letterToNumberWhite.get(firstLetter);
+        int secondNumber = letterToNumber.get(firstLetter);
         int firstNumber = Integer.parseInt(secondLetter);
         //System.out.printf("is at row %d and col %d on chessboard",firstNumber, secondNumber);
         return new ChessPosition(firstNumber, secondNumber);
